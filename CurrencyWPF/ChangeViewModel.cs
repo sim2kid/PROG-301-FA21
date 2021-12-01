d725ce1acbe13d613bfec8eb7aa27c690007eb86
sim2kid <sim2kid@users.noreply.github.com>
2021-11-30 20:09:55 -0600

    Populated ViewModel for CurrencyWPF

--- CurrencyWPF/ChangeViewModel.cs
+++ CurrencyWPF/ChangeViewModel.cs
@@ -3,10 +3,50 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Currency;
+using Currency.US;
+using Currency.JPY;
 
 namespace CurrencyWPF
 {
-    internal class ChangeViewModel
+    public class ChangeViewModel
     {
+        ICurrencyRepo repo;
+
+        public void MakeChange(Currency currency, float value) 
+        {
+            switch (currency) 
+            {
+                default:
+                case Currency.USD:
+                    repo = new USCurrencyRepo();
+                    break;
+                case Currency.JPY:
+                    repo = new JPYCurrencyRepo();
+                    break;
+            }
+            repo = repo.MakeChange(value);
+        }
+
+        public List<string> GetCoins() 
+        {
+            if (repo == null)
+                return new List<string>();
+
+            List<string> list = new List<string>();
+            foreach (ICoin coin in repo.Coins) 
+            {
+                list.Add(coin.GetType().ToString());
+            }
+            return list;
+        }
+
+    }
+
+
+    public enum Currency
+    {
+        USD,
+        JPY
     }
 }
--- CurrencyWPF/MainWindow.xaml.cs
+++ CurrencyWPF/MainWindow.xaml.cs
@@ -23,13 +23,26 @@ namespace CurrencyWPF
     public partial class MainWindow : Window
     {
         private static readonly Regex _float = new Regex(@"^[+-]?([0-9]+([.][0-9]*)?|[.][0-9]+)$");
+        private static readonly Regex _int = new Regex(@"^[0-9]+$");
+
+        ChangeViewModel model;
         private static bool IsFloat(string text)
         {
             return _float.IsMatch(text);
         }
+        private static bool IsInt(string text)
+        {
+            return _int.IsMatch(text);
+        }
+
+        bool useFloat, handle;
+        Currency currentCurrency;
 
         public MainWindow()
         {
+            model = new ChangeViewModel();
+            useFloat = true;
+            currentCurrency = Currency.USD;
             InitializeComponent();
         }
 
@@ -37,7 +50,67 @@ namespace CurrencyWPF
         {
             var box = ((TextBox)(e.Source));
             string full = box.Text.Insert(box.CaretIndex, e.Text);
-            e.Handled = !IsFloat(full);
+            if(useFloat)
+                e.Handled = !IsFloat(full);
+            else
+                e.Handled = !IsInt(full);
+        }
+
+        private void Currency_DropDownClosed(object sender, EventArgs e)
+        {
+            if (handle) Handle();
+            handle = true;
+        }
+
+        private void Currency_SelectionChanged(object sender, SelectionChangedEventArgs e) 
+        {
+            ComboBox cmb = sender as ComboBox;
+            handle = !cmb.IsDropDownOpen;
+            Handle();
+        }
+
+        private void NewCurrency(bool usesFloat, Currency currency)
+        {
+            currentCurrency = currency;
+            if (!usesFloat) 
+            {
+                txtChange.Text = txtChange.Text.ToString().Split('.')[0];
+            }
+            useFloat = usesFloat;
+        }
+
+        private void Handle() 
+        {
+            switch (currency.SelectedItem.ToString().Split(new string[] { ": " }, StringSplitOptions.None).Last())
+            {
+                case "$USD":
+                    NewCurrency(true, Currency.USD);
+                    break;
+                case "¥JPY":
+                    NewCurrency(false, Currency.JPY);
+                    break;
+            }
+        }
+
+        private void MakeChange(object sender, RoutedEventArgs e) 
+        {
+            float.TryParse(txtChange.Text, out float f);
+            model.MakeChange(currentCurrency, f);
+            ClearScreen();
+            FillScreen(model.GetCoins());
+        }
+
+        private void FillScreen(List<string> things) 
+        {
+            foreach (string s in things) 
+            {
+                lstContent.Items.Add(s);
+            }
+        }
+
+        private void ClearScreen() 
+        {
+            lstContent.Items.Clear();
         }
     }
 }
